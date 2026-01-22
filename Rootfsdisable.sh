e813649efe4cd9e3eb36495afcaa32fe29ae6392
StarkMist111960 <starkmist111960@gmail.com>
2026-01-22 14:48:54 -0700

    Create Rootfsdisable.sh to manage RootFS verification
    Added a script to disable RootFS verification with user prompt.

--- Rootfsdisable.sh
+++ Rootfsdisable.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+echo 'Welcome to the Pentagon Policy Editor RootFS Disabler!'
+read -p 'Disable RootFS verification (y, n)? ' ANS
+
+if [[ $ANS -eq 'y' ]]; then 
 
 sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
 sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
 echo 'RootFS has been disabled! (or already was)'
+
+else 
+  exit 1
+
+fi 
