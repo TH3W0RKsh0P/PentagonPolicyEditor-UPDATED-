dea38befa1746928cda3e2e71af2f92a4213ccff
StarkMist111960 <starkmist111960@gmail.com>
2026-01-21 18:27:07 -0700

    Add RootFS disabler script

--- Rootfsdisable.sh
+++ Rootfsdisable.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+echo 'Welcome to PentagonPolicyEditor RootFS Disabler'
+read -p 'Would you like to disable RootFS (y, n): ' ANS
+
+if [ $ANS = 'y' ]; then
+
+  sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 2
+  sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification --partitions 4
+  echo "RootFS has been disabled! (or already was)"
+
+elif [ $ANS = 'n' ]; then 
+  echo 'Now exiting...'
+  sleep 1
+  clear
+  exit 1
+  clear 
+  
+else
+  echo 'Invalid option, now exiting...'
+  sleep 1
+  clear
+  exit 1
+  clear
+fi
